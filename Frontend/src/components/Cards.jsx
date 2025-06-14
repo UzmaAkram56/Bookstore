af5203b61568f6fb9143fd594b37f1b4c3801242
Uzma Akram <uzma12970@gmail.com>
2025-06-14 11:21:22 +0500

    Frontend Complete Almost

---
+++ Frontend/src/components/Cards.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Cards({ item }) {
+  return (
+    <>
+      <div className="mt-4 my-3 p-2">
+        <div className="card bg-base-100 w-92 h-96 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dart:text-white dark:border">
+          <figure className="bg-blue-100">
+            <img src={item.image} alt="img"   className=" object-cover mix-blend-multiply"  />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-primary">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions flex justify-between">
+              <div className="badge badge-outline cursor-pointer  px-2 py-1 rounded-full border-[2px] hover:border-transparent hover:bg-blue-500 hover:text-white duration-200 border-gray-600 dark:border-white  dark:hover:border-black ">
+                ${item.price}
+              </div>
+              <div className="badge badge-outline cursor-pointer px-2 py-1 rounded-full border-[2px] hover:border-transparent hover:bg-blue-500 hover:text-white duration-200 border-gray-600 dark:border-white dark:hover:border-black">
+                Buy Now
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Cards from "./Cards";
+import list from "../../public/list.json";
+import {Link} from "react-router-dom";
+function Course() {
+  return (
+    <>
+    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 pt-24   '>
+      <div className=' items-center justify-center text-center  '>
+        <h1 className='text-2xl  md:text-4xl '>
+          Thank you for joining us — we're pleased to have you <span className='text-blue-700'>Here!!! :)</span>
+        </h1>
+        <p className='mt-12'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque corrupti et quos provident eum assumenda tempore beatae deserunt commodi at quibusdam expedita officiis, perferendis cupiditate cum, voluptatibus aut nemo harum blanditiis! Blanditiis, officiis, quas rem, consequuntur harum adipisci sequi expedita ipsa a eos ullam error ea asperiores quia placeat dolor tempore labore esse ut.
+        </p>
+      <Link to="/">
+        <button className=' mt-6 bg-blue-500 px-4 py-2 rounded-md hover:bg-blue-700 text-white'>Back</button>
+      </Link>
+        <div className='mt-12  grid grid-cols-1 md:grid-cols-4'>
+          {
+            list.map((item) => (
+              <Cards key={item.id} item = {item} />
+            ))
+          }
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Course
